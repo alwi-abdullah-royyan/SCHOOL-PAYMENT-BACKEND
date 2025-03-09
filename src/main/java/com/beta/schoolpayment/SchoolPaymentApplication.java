54b912dca314e6b1782c6fef1480eb14f453fde8
Alwi842 <alwi.abdullah1221@gmail.com>
2025-03-09 21:04:18 +0700

    init

---
+++ src/main/java/com/beta/schoolpayment/SchoolPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.beta.schoolpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beta/schoolpayment/SchoolPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beta.schoolpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
